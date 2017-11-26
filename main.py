6bb87dc87101a19c90b1c237a00591b5e084348e
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-11-26 13:39:32 -0500

    an app?

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sense_hat import SenseHat
+from random import randint
 import time
+import atexit
 
 s = SenseHat()
 s.low_light = True
@@ -12,88 +14,34 @@ white = (255,255,255)
 nothing = (0,0,0)
 pink = (255,105, 180)
 
-def trinket_logo():
-    G = green
+def grid_init():
     Y = yellow
     B = blue
     O = nothing
     logo = [
     O, O, O, O, O, O, O, O,
-    O, Y, Y, Y, B, G, O, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    Y, Y, Y, Y, Y, B, G, O,
-    O, Y, Y, Y, B, G, O, O,
+    O, O, O, O, O, O, O, O,
+    Y, Y, Y, B, O, O, O, B,
+    Y, O, Y, B, O, O, O, B,
+    Y, Y, Y, B, O, B, O, B,
+    Y, O, Y, B, B, O, B, B,
+    O, O, O, O, O, O, O, O,
     O, O, O, O, O, O, O, O,
     ]
     return logo
 
-def raspi_logo():
-    G = green
-    R = red
-    O = nothing
-    logo = [
-    O, G, G, O, O, G, G, O,
-    O, O, G, G, G, G, O, O,
-    O, O, R, R, R, R, O, O,
-    O, R, R, R, R, R, R, O,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    O, R, R, R, R, R, R, O,
-    O, O, R, R, R, R, O, O,
-    ]
-    return logo
+def grid_color(score=0):
+    c = red if score > 75 else yellow if score > 50 else blue if score > 25 else nothing
+    return [[c]*64][0]
 
-def plus():
-    W = white
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, W, W, O, O, O,
-    O, O, O, W, W, O, O, O,
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, W, W, O, O, O,
-    O, O, O, W, W, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
+def grid_clear():
+    grid_color()
 
-def equals():
-    W = white
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, W, W, W, W, W, W, O,
-    O, W, W, W, W, W, W, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
-
-def heart():
-    P = pink
-    O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, P, P, O, P, P, O, O,
-    P, P, P, P, P, P, P, O,
-    P, P, P, P, P, P, P, O,
-    O, P, P, P, P, P, O, O,
-    O, O, P, P, P, O, O, O,
-    O, O, O, P, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-    return logo
-
-images = [trinket_logo, trinket_logo, plus, raspi_logo, raspi_logo, equals, heart, heart]
-count = 0
+s.set_pixels(grid_init())
+time.sleep(.75)
 
 while True:
-    s.set_pixels(images[count % len(images)]())
+    s.set_pixels(grid_color(randint(0, 100)))
     time.sleep(.75)
-    count += 1
+
+atexit.register(grid_clear)
