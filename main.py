6a7a364be76c91ae7be22150c269a2e446ada8f5
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-11-27 21:58:40 -0500

    Reformatting code, removing unneeded functions

--- main.py
+++ main.py
@@ -9,34 +9,33 @@ green = (0, 255, 0)
 yellow = (255, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
-white = (255,255,255)
-nothing = (0,0,0)
+white = (255, 255, 255)
+nothing = (0, 0, 0)
 colors = [nothing, green, yellow, red]
 max = len(colors) - 1
 current = 1
 prev = 0
 off = 0
 
-def grid_init():
+
+def init():
     W = white
     B = blue
     O = nothing
-    logo = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    W, W, W, B, O, O, O, B,
-    W, O, W, B, O, O, O, B,
-    W, W, W, B, O, B, O, B,
-    W, O, W, B, B, O, B, B,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
+    logo = (
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        W, W, W, B, O, O, O, B,
+        W, O, W, B, O, O, O, B,
+        W, W, W, B, O, B, O, B,
+        W, O, W, B, B, O, B, B,
+        O, O, O, O, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+    )
     return logo
 
-def grid_draw(fill):
-    s.set_pixels(fill)
 
-def grid_color(pos=0):
+def color(pos=0):
     global current
 
     if pos > 0:
@@ -44,26 +43,24 @@ def grid_color(pos=0):
         prev = current
 
     current = pos
-    return [[colors[current]]*64][0]
+    return colors[current] * 64
 
-def grid_clear():
-    grid_draw(grid_color(0))
 
 try:
-    grid_draw(grid_init())
+    s.set_pixels(init())
     time.sleep(1)
-    grid_draw(grid_color(1))
+    s.set_pixels(color(1))
 
     while True:
         for event in s.stick.get_events():
             if event.action == 'released':
                 if event.direction == 'middle':
                     off ^= 1
-                    grid_draw(grid_color(0 if off == 1 else prev))
+                    s.set_pixels(color(0 if off == 1 else prev))
                 elif off == 0:
                     current = current + 1 if event.direction == 'up' else current - 1
                     current = max if current < 1 else 1 if current > max else current
-                    grid_draw(grid_color(current))
+                    s.set_pixels(color(current))
 
 except KeyboardInterrupt:
-    grid_clear()
+    s.set_pixels(color(0))
